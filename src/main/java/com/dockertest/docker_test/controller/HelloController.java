3d748770530e49e6d13b343c83692782f51acefd
BLADE <1012582116@qq.com>
2021-03-21 13:37:02 +0800

    Initial commit

---
+++ src/main/java/com/dockertest/docker_test/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.dockertest.docker_test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello Docker";
+  }
+}
